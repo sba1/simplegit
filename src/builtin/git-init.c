6395580790488090d72717c1741ddf72de676e7b
Marc Pegon <pe.marc@free.fr>
2011-05-23 12:10:27 +0200

    Added sample code for git-init using parse-options

--- src/builtin/git-init.c
+++ src/builtin/git-init.c
@@ -1,34 +1,76 @@
+#include <stdlib.h>
+#include "git2.h"
 #include "git-init.h"
 #include "git-support.h"
-
-int cmd_init(int argc, const char **argv){
-	//TO-DO !
-	(void)argc;
-	(void)argv;
-
-	please_git_do_it_for_me();
-}
-
-/*#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <git2.h>
+#include "parse-options.h"
 #include "errors.h"
 
+#define INIT_DB_QUIET 0x0001
 
-// Create an empty git repository or reinitialize an existing one
-int main(int argc, char *argv[]) {
-	if(argc != 2) {
-		printf("Path unspecified !\n");
-		return EXIT_FAILURE;
+static const char *const init_usage[] = {
+	"git init [-q | --quiet] [--bare] [--template=<template-directory>] [--shared[=<permissions>]] [directory]",
+	NULL
+};
+
+int cmd_init(int argc, const char **argv){
+	// WORK IN PROGRESS
+	// TODO : delete this line when finished
+	please_git_do_it_for_me();
+
+
+
+
+	const char *real_git_dir = NULL;
+	const char *template_dir = NULL;
+	unsigned int flags = 0;
+	int is_bare_repository_cfg = 0;
+	int init_shared_repository = -1;
+
+	const struct option init_db_options[] = {
+		OPT_STRING(0, "template", &template_dir, "template-directory",
+				"directory from which templates will be used"),
+		OPT_SET_INT(0, "bare", &is_bare_repository_cfg,
+				"create a bare repository", 1),
+		{ OPTION_CALLBACK, 0, "shared", &init_shared_repository,
+			"permissions",
+			"specify that the git repository is to be shared amongst several users",
+			PARSE_OPT_OPTARG | PARSE_OPT_NONEG, NULL/*shared_callback*/, 0},
+		OPT_BIT('q', "quiet", &flags, "be quiet", INIT_DB_QUIET),
+		OPT_STRING('L', "separate-git-dir", &real_git_dir, "gitdir",
+			   "separate git dir from working tree"),
+		OPT_END()
+	};
+
+	char prefix[1] = {0};
+	argc = parse_options(argc, argv, prefix, init_db_options, init_usage, 0);
+
+	if (argc == 0) {
+		/* Unimplemented : a directory must be specified for now */
+		please_git_do_it_for_me();
+	} else {
+		if (template_dir) {
+			/* Unimplemented : cannot specify a template directory yet */
+			please_git_do_it_for_me();
+		} else if (init_shared_repository != -1) {
+			/* Unimplemented : cannot create a shared repository yet */
+			please_git_do_it_for_me();
+		} else if (real_git_dir) {
+			/* Unimplemented : cannot create separate git dir yet */
+			please_git_do_it_for_me();
+		} else if (is_bare_repository_cfg) {
+			/* Unimplemented : cannot create bare repository yet */
+			please_git_do_it_for_me();
+		}
+
+		git_repository *repo;
+		int e = git_repository_init (&repo, argv[argc-1], 0);
+		if (e == GIT_ENOTIMPLEMENTED) {
+			please_git_do_it_for_me();
+		} else if (e) {
+			libgit_error(e);
+		}
 	}
-	
-	git_repository **repo_out;
-	//A voir : comment faire le test entre si on met true ou false
-	if (git_repository_init (repo_out, argv[argc-1], true) == GIT_ERROR) {
-		printf("Error : Can't create the repository");
-		break;
-	}
-	
+
 	return EXIT_SUCCESS;
-}*/
+}
+
