1f30d72f274b3fa270c869edf6dbc454adb5d7de
nassima <nassima@ubuntu.(none)>
2011-05-23 11:58:25 +0200

    Add git checkout (unterminated)

--- src/builtin.h
+++ src/builtin.h
@@ -7,13 +7,15 @@
 #include "git-init.h"
 #include "git-log.h"
 #include "git-ls-files.h"
+#include "git-checkout.h"
 
 int cmd_init(int argc, const char **argv);
 
 cmd_struct commands[] = {
 	{"init", cmd_init},
 	{"log", cmd_log},
-	{"ls-files", cmd_ls_files}
+	{"ls-files", cmd_ls_files},
+	{"checkout", cmd_checkout}
 };
 
 #endif /* BUILTIN_H */
---
+++ src/builtin/git-checkout.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <git2.h>
+#include "errors.h"
+#include "git-checkout.h"
+#include "git-support.h"
+
+
+int cmd_checkout(int argc, const char **argv) 
+{
+	if(argc != 1)
+		please_git_do_it_for_me();
+	
+	int e,i;
+	git_index *index;
+	git_repository *repo;
+	git_index_entry *index_entry;
+	git_oid id;
+	
+	 /* Open the repository */
+	e = git_repository_open(&repo, argv[argc-1]);
+	if (e) libgit_error(e);
+
+	/* Get the Index file of a Git repository */
+	e = git_repository_index(&index,repo);
+	if(e) libgit_error(e);
+	
+	/* Find the first index of any entries which point to given path in the Git index */
+	i = git_index_find (index, argv[argc-1]);
+	if(i) libgit_error(e);
+	else {
+	  /* get a pointer to one of the entries in the index */
+		index_entry = git_index_get(index, i);
+		if(index_entry == NULL) printf("Out of bound");
+		else id = index_entry->oid;
+	}
+	
+	git_reference *symbolic_ref;
+	if(git_reference_lookup(&symbolic_ref, repo, "HEAD")) libgit_error(e);
+
+	git_reference *direct_ref;
+	if(git_reference_resolve(&direct_ref, symbolic_ref)) libgit_error(e);
+
+	const git_oid *oid;
+	oid = git_reference_oid(direct_ref);
+	if (oid == NULL) {
+		printf("Internal error: reference is not direct\n");
+		return EXIT_FAILURE;
+	}
+	
+	git_tree *tree;
+	/* Lookup a tree object from the repository */
+	e = git_tree_lookup(&tree, repo, oid);
+	if(e) libgit_error(e);
+	
+	/* Update the index ?? */
+	e = git_index_read(index);
+	if(e) libgit_error(e);
+	
+	git_index_free(index);
+	git_repository_free(repo);
+	git_tree_close(tree);
+	
+	return 0;
+}
