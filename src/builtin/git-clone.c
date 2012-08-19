2888656417465a0d1a926ef8b29c627223416778
Chris Young <chris@unsatisfactorysoftware.co.uk>
2012-08-19 10:50:42 +0100

    import clone example from libgit2

--- src/builtin/git-clone.c
+++ src/builtin/git-clone.c
@@ -1,110 +1,68 @@
-#include "errors.h"
-#include <git2.h>
 #include "common.h"
+#include <git2.h>
+#include <git2/clone.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+struct dl_data {
+	git_indexer_stats fetch_stats;
+	git_indexer_stats checkout_stats;
+	git_checkout_opts opts;
+	int ret;
+	int finished;
+	const char *url;
+	const char *path;
+};
+
+static void *clone_thread(void *ptr)
+{
+	struct dl_data *data = (struct dl_data *)ptr;
+	git_repository *repo = NULL;
+
+	// Kick off the clone
+	data->ret = git_clone(&repo, data->url, data->path,
+								 &data->fetch_stats, &data->checkout_stats, 
+								 &data->opts);
+	if (repo) git_repository_free(repo);
+	data->finished = 1;
+
+	pthread_exit(&data->ret);
+}
 
 int clone(int argc, char **argv)
-{   
-	// Let's suppose the URL looks like: https://github.com/libgit2/libgit2.git
-	// Then we need to get a URL like this too: git://github.com/libgit2/libgit2.git
-	// This may be a bit dodgy, but it will do for now.
-	//const char *gitURL = [remoteURL.absoluteString stringByReplacingOccurrencesOfString:@"https://github.com/" withString:@"git://github.com/"].UTF8String;
+{
+	struct dl_data data = {0};
+	pthread_t worker;
 
-	//Setup
-	int error;
-	git_repository *repo;
-	git_config *cfg;
-	git_remote *remote;
-
-	error = git_repository_open(&repo, ".git");
-	
-	if (error != GIT_SUCCESS) {
-		git_repository_init(&repo, ".git", 1);
-
-		//Config
-		git_repository_config(&cfg, repo);
-		git_config_set_int32 (cfg, "core.repositoryformatversion", 0);
-		git_config_set_bool (cfg, "core.filemode", 1);
-		git_config_set_bool (cfg, "core.bare", 0);
-		git_config_set_bool (cfg, "core.logallrefupdates", 1);
-		git_config_set_bool (cfg, "core.ignorecase", 1);
-		git_config_set_string (cfg, "remote.origin.url", argv[1]);
-		git_config_set_string (cfg, "remote.origin.fetch", "+refs/heads/*:refs/remotes/origin/*");
-		git_config_set_string (cfg, "branch.master.remote", "origin");
-		git_config_set_string (cfg, "branch.master.merge", "refs/heads/master");
-
-		git_repository_set_workdir(repo, ".git");
-
-		error = git_remote_new(&remote, repo, "A remote", argv[1], "origin");
-
-		git_repository_free(repo);
-		git_repository_open (&repo, ".git");
-	}
-
-	git_repository_config(&cfg, repo);
-
-	// connect to repo
-	error = git_remote_load(&remote, repo, "origin");
-
-	error = git_remote_connect(remote, GIT_DIR_FETCH);
-	// get pack file
-
-	git_off_t bytes;
-	git_indexer_stats stats;
-	error = git_remote_download(remote, &bytes, &stats);
-
-	char *packFolderURL = ".git/objects/pack";
-	char *packname = NULL;
-	DIR *dir = NULL;
-	struct dirent *de;
-
-	if ((dir = opendir(packFolderURL)) == NULL) {
-		giterr_set(GITERR_OS, "Failed to open directory '%s'", packFolderURL);
+	// Validate args
+	if (argc < 3) {
+		printf("USAGE: %s <url> <path>\n", argv[0]);
 		return -1;
 	}
 
-	while(de = readdir(dir))
-	{
-		if(strstr(de->d_name, ".pack") == NULL) continue;
-		
-		asprintf(&packname, "%s/%s", packFolderURL, de->d_name);
-		printf("packfile: %s\n", packname);
-		
-		// unpack pack file
-		if (packname != NULL)
-		{
-			git_indexer *indexer;
-			git_indexer_stats stats2;
-			int error;
-			char hash[GIT_OID_HEXSZ + 1] = {0};
-			char *indexPath = NULL;
-	
-			error = git_indexer_new(&indexer, packname);
-			error = git_indexer_run(indexer, &stats2);
-			error = git_indexer_write(indexer);
+	// Data for background thread
+	data.url = argv[1];
+	data.path = argv[2];
+	data.opts.disable_filters = 1;
+	printf("Cloning '%s' to '%s'\n", data.url, data.path);
 
-			// Get the packfile's hash (which should become it's filename)
-			git_oid_fmt(hash, git_indexer_hash(indexer));
+	// Create the worker thread
+	pthread_create(&worker, NULL, clone_thread, &data);
 
-			asprintf(&indexPath, "%s/pack-%s.idx", packFolderURL, hash);
+	// Watch for progress information
+	do {
+		usleep(10000);
+		printf("Fetch %d/%d  –  Checkout %d/%d\n",
+				 data.fetch_stats.processed, data.fetch_stats.total,
+				 data.checkout_stats.processed, data.checkout_stats.total);
+	} while (!data.finished);
+	printf("Fetch %d/%d  –  Checkout %d/%d\n",
+			 data.fetch_stats.processed, data.fetch_stats.total,
+			 data.checkout_stats.processed, data.checkout_stats.total);
 
-			puts(hash);
-			git_index *index;
-			git_index_open(&index, indexPath);
-			git_index_read(index);
-			git_repository_set_index(repo, index);
-
-			git_indexer_free(indexer);
-			git_remote_update_tips(remote, update_cb); //No idea what it does, but it seems like it's important… It does make the branches appear in .git/refs/remotes/origin
-
-			free(indexPath);
-			free(packname);
-		}
-	}
-	
-	if(dir) closedir(dir);
-	
-//Somehow do git checkout master here
-
-return 0;
-
+	return data.ret;
 }
+
