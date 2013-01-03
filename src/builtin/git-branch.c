e2a0b4b3098479a1b38546a9c243b1fc7a74fd7a
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-03 09:18:31 +0100

    Now support -r and -a arguments to support all listing all remote branches and all branches.

--- src/builtin/git-branch.c
+++ src/builtin/git-branch.c
@@ -12,9 +12,17 @@ static int branch_list_callback(const char *branch_name, git_branch_t branch_typ
 	return 0;
 }
 
-
 int cmd_branch(git_repository *repo, int argc, char **argv)
 {
-	git_branch_foreach(repo,GIT_BRANCH_LOCAL,branch_list_callback,NULL);
+	int i;
+	unsigned int list_flags = GIT_BRANCH_LOCAL;
+
+	for (i=1;i<argc;i++)
+	{
+		if (!strcmp(argv[i],"-a")) list_flags = GIT_BRANCH_LOCAL | GIT_BRANCH_REMOTE;
+		else if (!strcmp(argv[i],"-r")) list_flags = GIT_BRANCH_REMOTE;
+	}
+
+	git_branch_foreach(repo,list_flags,branch_list_callback,NULL);
 	return EXIT_SUCCESS;
 }
