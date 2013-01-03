57f40d9150fb5f4db25da0a1a624c6d70eb80154
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-03 17:50:54 +0100

    Implemented the creation of branches.

--- src/builtin/git-branch.c
+++ src/builtin/git-branch.c
@@ -5,6 +5,7 @@
 #include <git2.h>
 
 #include "git-checkout.h"
+#include "git-support.h"
 
 struct branch_list_callback_payload
 {
@@ -29,19 +30,48 @@ int cmd_branch(git_repository *repo, int argc, char **argv)
 {
 	int i;
 	unsigned int list_flags;
-	struct branch_list_callback_payload pl;
+	char *branch = NULL;
 
-	list_flags = GIT_BRANCH_LOCAL;
+	list_flags = 0;
 
 	for (i=1;i<argc;i++)
 	{
-		if (!strcmp(argv[i],"-a")) list_flags = GIT_BRANCH_LOCAL | GIT_BRANCH_REMOTE;
+		if (argv[i][0] != '-')
+		{
+			branch = argv[i];
+		}
+		else if (!strcmp(argv[i],"-a")) list_flags = GIT_BRANCH_LOCAL | GIT_BRANCH_REMOTE;
 		else if (!strcmp(argv[i],"-r")) list_flags = GIT_BRANCH_REMOTE;
 	}
 
-	pl.repo = repo;
+	if (branch && list_flags)
+	{
+		fprintf(stderr,"Options -a and -r do not make sense with a branch name");
+		return EXIT_FAILURE;
 
-	git_branch_foreach(repo,list_flags,branch_list_callback,&pl);
+	}
+
+	if (!branch)
+	{
+		struct branch_list_callback_payload pl;
+		pl.repo = repo;
+		git_branch_foreach(repo,list_flags,branch_list_callback,&pl);
+	} else
+	{
+		int err;
+
+		git_reference *head_ref;
+
+		if ((err = git_repository_head(&head_ref,repo)) == 0)
+		{
+			git_reference *new_branch_ref;
+			if ((err = git2_create_branch_from_ref(&new_branch_ref,head_ref,repo,branch)) == 0)
+			{
+				git_reference_free(new_branch_ref);
+			}
+			git_reference_free(head_ref);
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
