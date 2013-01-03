4a0d47c27dfe760f89ca42dc4d1fa0f9cf1677b7
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-03 17:34:16 +0100

    Refactored branch creation from reference into an own function.

--- src/builtin/git-checkout.c
+++ src/builtin/git-checkout.c
@@ -12,6 +12,33 @@ static int conflict_cb(const char *conflicting_path, const git_oid *index_oid, u
 	return 1;
 }
 
+/**
+ * Create a branch with a given branch name from a reference.
+ *
+ * @param new_branch_ref where the reference of the new branch is stored to.
+ * @param branch_ref the ref which to branch.
+ * @param repo the repo
+ * @param branch the name of the new branch
+ * @return error code or 0 for success.
+ */
+int git2_create_branch_from_ref(git_reference **new_branch_ref, git_reference *branch_ref, git_repository* repo, const char *branch)
+{
+	int err;
+
+	git_object *commit;
+	git_commit *comm;
+
+	if ((err = git_reference_peel(&commit, branch_ref, GIT_OBJ_COMMIT)) != 0)
+		goto out;
+
+	if ((err = git_commit_lookup(&comm, repo, git_object_id(commit))) != 0)
+		goto out;
+
+	err = git_branch_create(new_branch_ref, repo, branch, comm, 0);
+out:
+	return err;
+}
+
 int cmd_checkout(git_repository *repo, int argc, char **argv)
 {
 	int i, err, rc;
@@ -36,39 +63,19 @@ int cmd_checkout(git_repository *repo, int argc, char **argv)
 	if (git_branch_lookup(&branch_ref,repo,branch,GIT_BRANCH_LOCAL) != 0)
 	{
 		/* No, try remote branch */
+		git_reference *remote_branch_ref;
 		char remote_buf[256];
 		snprintf(remote_buf,sizeof(remote_buf),"%s/%s","origin",branch);
-		if (git_branch_lookup(&branch_ref,repo,remote_buf,GIT_BRANCH_REMOTE) == 0)
+		if (git_branch_lookup(&remote_branch_ref, repo, remote_buf, GIT_BRANCH_REMOTE) == 0)
 		{
-			/* Exists, now try to create a local one */
-			int err;
-			git_object *commit;
-			if ((err = git_reference_peel(&commit,branch_ref,GIT_OBJ_COMMIT)) == 0)
-			{
-				git_commit *comm;
-				if ((rc = git_commit_lookup(&comm,repo,git_object_id(commit)) == 0))
-				{
-					if (git_branch_create(&branch_ref, repo, branch, comm, 0) == 0)
-					{
-						/* TODO: Alter tracking config */
-					} else
-					{
-						fprintf(stderr,"Error code %d\n",err);
-						libgit_error();
-						goto out;
-					}
-				} else
-				{
-					fprintf(stderr,"Error code %d\n",err);
-					libgit_error();
-					goto out;
-				}
-			} else
+			/* Exists, now try to create a local one from that reference */
+			if ((err = git2_create_branch_from_ref(&branch_ref,remote_branch_ref,repo,branch)) != 0)
 			{
 				fprintf(stderr,"Error code %d\n",err);
 				libgit_error();
 				goto out;
 			}
+			git_reference_free(remote_branch_ref);
 		} else
 		{
 			branch_ref = NULL;
