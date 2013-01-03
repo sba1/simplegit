99e7c0fa6e6959eae03b3e3bdb1d6ff8f50438da
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-03 10:06:29 +0100

    Mark current branch with *.

--- src/builtin/git-branch.c
+++ src/builtin/git-branch.c
@@ -6,16 +6,32 @@
 
 #include "git-checkout.h"
 
+struct branch_list_callback_payload
+{
+	git_repository *repo;
+};
+
 static int branch_list_callback(const char *branch_name, git_branch_t branch_type, void *payload)
 {
-	printf("%s\n",branch_name);
+	struct branch_list_callback_payload *pl = (struct branch_list_callback_payload*)payload;
+	struct git_reference *branch_ref;
+
+	if (git_branch_lookup(&branch_ref,pl->repo,branch_name,branch_type) != 0)
+		branch_ref = NULL;
+
+	printf("%s%s\n",git_branch_is_head(branch_ref)?"* ":"  ",branch_name);
+
+	git_reference_free(branch_ref);
 	return 0;
 }
 
 int cmd_branch(git_repository *repo, int argc, char **argv)
 {
 	int i;
-	unsigned int list_flags = GIT_BRANCH_LOCAL;
+	unsigned int list_flags;
+	struct branch_list_callback_payload pl;
+
+	list_flags = GIT_BRANCH_LOCAL;
 
 	for (i=1;i<argc;i++)
 	{
@@ -23,6 +39,9 @@ int cmd_branch(git_repository *repo, int argc, char **argv)
 		else if (!strcmp(argv[i],"-r")) list_flags = GIT_BRANCH_REMOTE;
 	}
 
-	git_branch_foreach(repo,list_flags,branch_list_callback,NULL);
+	pl.repo = repo;
+
+	git_branch_foreach(repo,list_flags,branch_list_callback,&pl);
+
 	return EXIT_SUCCESS;
 }
