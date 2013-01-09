88ed4f6b513ac8411d0caf65c5afba0e4e5c4838
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-09 22:13:40 +0100

    Added support for enqueuing commits using parent commit oids. This fixes another test failure.

--- src/builtin/git-commit-tree.c
+++ src/builtin/git-commit-tree.c
@@ -33,23 +33,37 @@ int cmd_commit_tree(git_repository *repo, int argc, char **argv)
 	git_signature *author_signature = NULL;
 	git_signature *committer_signature = NULL;
 	git_tree *tree = NULL;
+	git_commit **parents = NULL;
 
-	int i;
+	int i,p;
 	int rc = EXIT_FAILURE;
+	int num_parents = 0;
 
 	for (i=1;i<argc;i++)
 	{
-		if (argv[i][0] == '-')
+		if (!strcmp(argv[i],"-p"))
+		{
+			if (++i < argc)
+			{
+				num_parents++;
+			} else
+			{
+				fprintf(stderr,"Option -p misses argument\n");
+				goto out;
+			}
+		}
+		else if (argv[i][0] == '-')
 		{
 			fprintf(stderr,"Unknown option \"%s\"\n",argv[i]);
 			goto out;
-		}
-
-		if (!tree_arg) tree_arg = argv[i];
-		else
+		} else
 		{
-			fprintf(stderr,"Only a single tree is accepted for now\n");
-			goto out;
+			if (!tree_arg) tree_arg = argv[i];
+			else
+			{
+				fprintf(stderr,"Only a single tree for is accepted for committing\n");
+				goto out;
+			}
 		}
 	}
 
@@ -83,6 +97,29 @@ int cmd_commit_tree(git_repository *repo, int argc, char **argv)
 		}
 	}
 
+	if (!(parents = malloc(sizeof(*parents)*num_parents)))
+	{
+		fprintf(stderr,"Not enough memory!\n");
+		goto out;
+	}
+
+	p = 0;
+	for (i=1;i<argc;i++)
+	{
+		if (!strcmp(argv[i],"-p"))
+		{
+			git_oid parent_oid;
+
+			/* This has to exists */
+			i++;
+			if ((err = git_oid_fromstr(&parent_oid,argv[i])) != GIT_OK)
+				goto out;
+			if ((err = git_commit_lookup_prefix(&parents[p],repo,&parent_oid,strlen(argv[i]))) != GIT_OK)
+				goto out;
+			p++;
+		}
+	}
+
 	if ((err = git_oid_fromstr(&tree_oid,tree_arg)) != GIT_OK)
 		goto out;
 	if ((err = git_tree_lookup_prefix(&tree,repo,&tree_oid,strlen(tree_arg))) != GIT_OK)
@@ -99,7 +136,7 @@ int cmd_commit_tree(git_repository *repo, int argc, char **argv)
 	}
 
 	if ((err = git_commit_create(&commit_oid, repo, NULL, author_signature, committer_signature,
-				NULL, buffer.buf, tree, 0, NULL)) != GIT_OK)
+				NULL, buffer.buf, tree, num_parents, (const git_commit**)parents)) != GIT_OK)
 		goto out;
 
 	printf("%s\n", git_oid_tostr(commit_oid_string, sizeof(commit_oid_string), &commit_oid));
@@ -108,6 +145,7 @@ int cmd_commit_tree(git_repository *repo, int argc, char **argv)
 out:
 	if (err) libgit_error();
 	if (tree) git_tree_free(tree);
+	if (parents) free(parents);
 	if (author_signature) git_signature_free(author_signature);
 	if (committer_signature) git_signature_free(committer_signature);
 	strbuf_release(&buffer);
