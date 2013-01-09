01009ee51b5c06dae298c76a44626fc4a1a04f84
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-09 14:08:27 +0100

    Refactored the printing of commit details into an own function.

--- src/builtin/git-log.c
+++ src/builtin/git-log.c
@@ -4,8 +4,51 @@
 
 #include <git2.h>
 
+#include "git-show.h"
 #include "git-log.h"
 #include "git-support.h"
+
+static void print_commit(struct git_commit *wcommit)
+{
+	const char *cmsg;
+	char c;
+	int nl;
+	const git_signature *cauth;
+	char oid_str[80];
+	char t[40];
+	time_t tt;
+	struct tm tm;
+	const git_oid *woid;
+
+	woid = git_commit_id(wcommit);
+
+	git_oid_tostr(oid_str,sizeof(oid_str),woid);
+
+	cmsg  = git_commit_message(wcommit);
+	cauth = git_commit_author(wcommit);
+	tt = cauth->when.time + cauth->when.offset*60;
+	tm = *gmtime(&tt);
+	tm.tm_gmtoff = cauth->when.offset*60;
+	strftime(t,sizeof(t),"%a %d %b %Y %T %z",&tm);
+
+	printf("commit %s\n",oid_str);
+	printf("Author: %s <%s>\n",cauth->name,cauth->email);
+	printf("Date:   %s\n",t);
+	printf("\n");
+
+	nl = 1;
+	while ((c = *cmsg++))
+	{
+		if (nl)
+		{
+			printf("    ");
+			nl = 0;
+		}
+		putc(c,stdout);
+		if (c == '\n') nl = 1;
+	}
+	printf("\n");
+}
 
 int cmd_log(git_repository *repo, int argc, char **argv)
 {
@@ -26,43 +69,12 @@ int cmd_log(git_repository *repo, int argc, char **argv)
 	while ((git_revwalk_next(&oid, walk)) == 0)
 	{
 		struct git_commit *wcommit;
-		const char *cmsg;
-		char c;
-		int nl;
-		const git_signature *cauth;
-		char oid_str[80];
-		char t[40];
-		time_t tt;
-		struct tm tm;
 
 		if (git_commit_lookup(&wcommit, repo, &oid) != 0)
 			continue;
-		git_oid_tostr(oid_str,sizeof(oid_str),&oid);
 
-		cmsg  = git_commit_message(wcommit);
-		cauth = git_commit_author(wcommit);
-		tt = cauth->when.time + cauth->when.offset*60;
-		tm = *gmtime(&tt);
-		tm.tm_gmtoff = cauth->when.offset*60;
-		strftime(t,sizeof(t),"%a %d %b %Y %T %z",&tm);
+		print_commit(wcommit);
 
-		printf("commit %s\n",oid_str);
-		printf("Author: %s <%s>\n",cauth->name,cauth->email);
-		printf("Date:   %s\n",t);
-		printf("\n");
-
-		nl = 1;
-		while ((c = *cmsg++))
-		{
-			if (nl)
-			{
-				printf("    ");
-				nl = 0;
-			}
-			putc(c,stdout);
-			if (c == '\n') nl = 1;
-		}
-		printf("\n");
 		git_commit_free(wcommit);
 	}
 
