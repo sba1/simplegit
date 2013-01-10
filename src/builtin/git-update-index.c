0f1906ac98fb3fc78ff448ad36f6d1d33d399f44
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-10 18:38:44 +0100

    Implemented --replace option.

--- src/builtin/git-update-index.c
+++ src/builtin/git-update-index.c
@@ -8,8 +8,70 @@
 
 #include "errors.h"
 #include "repository.h"
-
+#include "strbuf.h"
 #include "git-support.h"
+
+/**
+ * Returns whether the given path is part of a directory in the given index.
+ *
+ * @param idx
+ * @param file
+ * @return
+ */
+static int does_path_overlap_with_a_directory_in_index(git_index *idx, char *path)
+{
+	size_t i;
+	size_t entries;
+
+	/* If the path is contained in the index, then by assumption,
+	 * it cannot be a directory.
+	 */
+	if (git_index_get_bypath(idx, path, 0))
+		return 0;
+
+	entries = git_index_entrycount(idx);
+
+	/* Slow because we have to check each entry */
+	for (i=0;i<entries;i++)
+	{
+		const git_index_entry *ie;
+
+		ie = git_index_get_byindex(idx,i);
+
+		if (!prefixcmp(ie->path,path) && ie->path[strlen(path)] == '/')
+			return 1;
+	}
+
+	return 0;
+}
+
+/**
+ * Returns the index entry that is a prefix of the given path.
+ *
+ * @param idx
+ * @param path
+ * @return the index entry or NULL.
+ */
+static const git_index_entry *find_index_entry_that_is_a_prefix_of_path(git_index *idx, char *path)
+{
+	size_t i;
+	size_t entries;
+
+	entries = git_index_entrycount(idx);
+
+	/* Slow because we have to check each entry */
+	for (i=0;i<entries;i++)
+	{
+		const git_index_entry *ie;
+
+		ie = git_index_get_byindex(idx,i);
+
+		if (!prefixcmp(path,ie->path) && path[strlen(ie->path)] == '/')
+			return ie;
+	}
+
+	return NULL;
+}
 
 int cmd_update_index(git_repository *repo, int argc, char **argv)
 {
@@ -23,12 +85,14 @@ int cmd_update_index(git_repository *repo, int argc, char **argv)
 
 	int add_given = 0;
 	int remove_given = 0;
+	int replace_given = 0;
 	int index_info_given = 0;
 
 	for (i=1;i<argc;i++)
 	{
 		if (!strcmp(argv[i], "--add")) add_given = 1;
 		else if (!strcmp(argv[i], "--remove")) remove_given = 1;
+		else if (!strcmp(argv[i], "--replace")) replace_given = 1;
 		else if (!strcmp(argv[i], "--index-info")) index_info_given = 1;
 		else if (!strcmp(argv[i], "--")) { i++; break; }
 		else if (argv[i][0] != '-') break;
@@ -48,32 +112,58 @@ int cmd_update_index(git_repository *repo, int argc, char **argv)
 
 	for (int i = 0; i < filec; i++)
 	{
-		int file_exists;
+		int file_exists_in_wd;
+		int file_exists_in_index;
 		struct stat st;
 
 		snprintf(complete_path,sizeof(complete_path),"%s%s",get_git_prefix(),filev[i]);
+		file_exists_in_index = !!git_index_get_bypath(idx, complete_path, 0);
 
-		if (!add_given)
+		if (lstat(complete_path,&st) == 0) file_exists_in_wd = 1;
+		else file_exists_in_wd = 0;
+
+		if (!add_given && !file_exists_in_index)
 		{
-			if (git_index_find(idx, complete_path) < 0)
-			{
-				printf("error: %s: cannot add to the index - missing --add option?\n", filev[i]);
-				printf("fatal: Unable to process path %s\n", filev[i]);
-				goto out;
-			}
+			printf("error: %s: cannot add to the index - missing --add option?\n", filev[i]);
+			printf("fatal: Unable to process path %s\n", filev[i]);
+			goto out;
 		}
 
-		if (lstat(complete_path,&st) == 0) file_exists = 1;
-		else file_exists = 0;
-
-		if (!remove_given && !file_exists)
+		if (!remove_given && !file_exists_in_wd)
 		{
 			printf("error: %s: does not exists and --remove not passed\n", filev[i]);
 			printf("fatal: Unable to process path %s\n", filev[i]);
 			goto out;
 		}
 
-		if (file_exists)
+		if (file_exists_in_wd)
+		{
+			const git_index_entry *ie;
+
+			if ((!(st.st_mode & __S_IFDIR)) && does_path_overlap_with_a_directory_in_index(idx,complete_path))
+			{
+				if (!replace_given)
+				{
+					fprintf(stderr,"File %s is contained as a directory in the index!\n",complete_path);
+					goto out;
+				}
+				if ((err = git_index_remove_directory(idx,complete_path,0)) != GIT_OK)
+					goto out;
+			}
+
+			if ((ie = find_index_entry_that_is_a_prefix_of_path(idx,complete_path)))
+			{
+				if (!replace_given)
+				{
+					fprintf(stderr,"The path %s extends a file in the index!\n",complete_path);
+					goto out;
+				}
+				if ((err = git_index_remove(idx,ie->path,0)) != GIT_OK)
+					goto out;
+			}
+		}
+
+		if (file_exists_in_wd)
 		{
 			if ((err = git_index_add_from_workdir(idx, complete_path)) != GIT_OK)
 				goto out;
