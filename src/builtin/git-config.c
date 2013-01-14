3f78c216cb63b923e10ba574b9e28bd31e7265a5
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-14 11:47:17 +0100

    Implemented getting of repo config

--- src/builtin/git-config.c
+++ src/builtin/git-config.c
@@ -16,10 +16,50 @@ int cmd_config(git_repository *repo, int argc, char **argv)
 	int rc = EXIT_FAILURE;
 	int err = 0;
 
-	for (i=1;i<argc;i++)
+	const char *name = NULL;
+	const char *val = NULL;
+
+	git_config *config;
+
+	if (repo)
 	{
+		if ((err = git_repository_config(&config,repo)) != GIT_OK)
+			goto out;
+	} else
+	{
+		fprintf(stderr,"Config needs a repo for now\n!");
 		goto out;
 	}
+
+	for (i=1;i<argc;i++)
+	{
+		if (argv[i][0] != '-')
+		{
+			if (!name) name = argv[i];
+			else if (!val) val = argv[i];
+			else
+			{
+				fprintf(stderr,"Invalid or not supported command line format!\n");
+				goto out;
+			}
+		}
+	}
+
+	if (!name)
+	{
+		fprintf(stderr,"Invalid or not supported command line format!\n");
+		goto out;
+	}
+
+	if (val)
+	{
+		fprintf(stderr,"Setting config vals is not supported for now!\n");
+		goto out;
+	}
+
+	if ((err = git_config_get_string(&val,config,name)) != GIT_OK)
+		goto out;
+	printf("%s\n",val);
 
 	rc = EXIT_SUCCESS;
 out:
