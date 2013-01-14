87ac86dbf27d4b80b7a2de31813fb0c43cf5a208
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-14 17:06:07 +0100

    Various fixes.
    Do not free the supplied repo as we are not the owner of it.
    Return proper return code on success.
    Fail, if a unsupport option is supplied (all currently).

--- src/builtin/git-add.c
+++ src/builtin/git-add.c
@@ -12,7 +12,7 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 	int e,i;
 	int rc = EXIT_FAILURE;
 	const char *wd;
-	git_index *idx;
+	git_index *idx = NULL;
 	int num_added = 0;
 	int wd_len;
 
@@ -36,23 +36,32 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 				{
 					int path_skip = wd_len;
 					while (path[path_skip]=='/') path_skip++;
-					if (git_index_add_from_workdir(idx,path) == 0)
-					{
-						num_added++;
-					} else libgit_error();
-				} else fprintf(stderr,"%s is outside repository!\n",argv[i]);
+
+					if (!(git_index_add_from_workdir(idx,path) == GIT_OK))
+						goto out;
+					num_added++;
+				} else
+				{
+					fprintf(stderr,"%s is outside repository!\n",argv[i]);
+				}
 				free(path);
 			}
+		} else
+		{
+			fprintf(stderr,"Unknown option \"%s\"\n",argv[i]);
+			goto out;
 		}
 	}
 
 	if (num_added)
-		git_index_write(idx);
+	{
+		if ((e = git_index_write(idx)) != GIT_OK)
+			goto out;
+	}
 
-	git_index_free(idx);
-
+	rc = EXIT_SUCCESS;
 out:
-	if (repo) git_repository_free(repo);
+	if (idx) git_index_free(idx);
 	if (e != GIT_OK)
 		libgit_error();
 	return rc;
