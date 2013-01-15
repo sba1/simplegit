9921f0036f8fe0ac4ca902e234574a0d1f9ac19d
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-15 14:05:36 +0100

    Now works again. Be more chatty in case of errors.

--- src/builtin/git-add.c
+++ src/builtin/git-add.c
@@ -9,7 +9,8 @@
 
 int cmd_add(git_repository *repo, int argc, char **argv)
 {
-	int e,i;
+	int err = GIT_OK;
+	int i;
 	int rc = EXIT_FAILURE;
 	const char *wd;
 	git_index *idx = NULL;
@@ -17,11 +18,14 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 	int wd_len;
 
 	if (!(wd = git_repository_workdir(repo)))
+	{
+		fprintf(stderr,"Couldn't determine workdir!\n");
 		goto out;
+	}
 
 	wd_len = strlen(wd);
 
-	if ((e = git_repository_index(&idx,repo)) != GIT_OK)
+	if ((err = git_repository_index(&idx,repo)) != GIT_OK)
 		goto out;
 
 	for (i=1;i<argc;i++)
@@ -37,7 +41,7 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 					int path_skip = wd_len;
 					while (path[path_skip]=='/') path_skip++;
 
-					if (!(git_index_add_from_workdir(idx,path) == GIT_OK))
+					if ((err = git_index_add_from_workdir(idx,&path[path_skip])) != GIT_OK)
 						goto out;
 					num_added++;
 				} else
@@ -45,6 +49,9 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 					fprintf(stderr,"%s is outside repository!\n",argv[i]);
 				}
 				free(path);
+			} else
+			{
+				fprintf(stderr,"Couldn't determine absolute path of %s!\n",argv[i]);
 			}
 		} else
 		{
@@ -55,14 +62,14 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 
 	if (num_added)
 	{
-		if ((e = git_index_write(idx)) != GIT_OK)
+		if ((err = git_index_write(idx)) != GIT_OK)
 			goto out;
 	}
 
 	rc = EXIT_SUCCESS;
 out:
 	if (idx) git_index_free(idx);
-	if (e != GIT_OK)
+	if (err != GIT_OK)
 		libgit_error();
 	return rc;
 }
