289cf2346445890b09c29c9a24d04a078c5d7ad5
Sebastian Bauer <mail@sebastianbauer.info>
2013-01-27 18:02:05 +0100

    Removed the current usage of git_diff_index_to_workdir().
    The function git_diff_index_to_workdir() always loads the workdir from
    disk for index entries whose stats don't match (i.e., after a call to
    read-tree). The now implemented way should correspond better the with
    the behaviour of original git, which respond by using a 0 oid in this
    situation.

--- src/builtin/git-diff-files.c
+++ src/builtin/git-diff-files.c
@@ -11,8 +11,34 @@
 
 static int file_cb(const git_diff_delta *delta, float progress, void *payload)
 {
-	printf("%s\n",delta->new_file.path);
+	char ooid_str[GIT_OID_HEXSZ+1];
+	char noid_str[GIT_OID_HEXSZ+1];
+	char status;
+
+	progress = progress;
+	payload = payload;
+
+	git_oid_tostr(noid_str,sizeof(noid_str),&delta->new_file.oid);
+	git_oid_tostr(ooid_str,sizeof(ooid_str),&delta->old_file.oid);
+	switch (delta->status)
+	{
+		case	GIT_DELTA_MODIFIED: status = 'M'; break;
+		case	GIT_DELTA_DELETED: status = 'D'; break;
+		default: status = '?'; break;
+	}
+	printf(":%06o %06o %s %s %c\t%s\n",delta->new_file.mode,delta->old_file.mode,noid_str,ooid_str,status,delta->new_file.path);
 	return 0;
+}
+
+static int sgit_get_mode(int mode)
+{
+	if (S_ISLNK(mode))
+		return S_IFLNK;
+
+	if (S_ISDIR(mode) || (mode & S_IFMT) == (S_IFLNK | S_IFDIR))
+		return (S_IFLNK | S_IFDIR);
+
+	return S_IFREG | ((mode & 0100) ? 0755 : 0644);
 }
 
 int cmd_diff_files(git_repository *repo, int argc, char **argv)
@@ -23,10 +49,11 @@ int cmd_diff_files(git_repository *repo, int argc, char **argv)
 	int filec = 0;
 	char **filev = NULL;
 
-	git_index *idx = NULL;
-	git_diff_list *d = NULL;
+	size_t index_entries;
 
-	git_diff_options opts;
+	git_index *idx = NULL;
+
+	git_diff_delta delta;
 
 	for (i=1;i<argc;i++)
 	{
@@ -47,14 +74,34 @@ int cmd_diff_files(git_repository *repo, int argc, char **argv)
 	if ((err = git_repository_index(&idx,repo)) != GIT_OK)
 		goto out;
 
-	memset(&opts,0,sizeof(opts));
-	opts.version = GIT_DIFF_OPTIONS_VERSION;
+	memset(&delta,0,sizeof(delta));
+	index_entries = git_index_entrycount(idx);
 
-	if ((err = git_diff_index_to_workdir(&d,repo,idx,&opts)) != GIT_OK)
-		goto out;
+	for (i=0;i<(int)index_entries;i++)
+	{
+		const git_index_entry *e;
+		struct stat st;
 
-	if ((err = git_diff_foreach(d,file_cb,NULL,NULL,&opts)) != GIT_OK)
-		goto out;
+		e = git_index_get_byindex(idx,i);
+
+		delta.new_file.path = e->path;
+		delta.new_file.oid = e->oid;
+		delta.new_file.mode = e->mode;
+
+		if (!lstat(e->path,&st))
+		{
+			delta.old_file.path = e->path;
+			delta.old_file.mode = sgit_get_mode(st.st_mode);
+			git_oid_fromstrn(&delta.old_file.oid,"",0);
+			delta.status = GIT_DELTA_MODIFIED;
+		} else
+		{
+			delta.status = GIT_DELTA_DELETED;
+			delta.old_file.mode = 0;
+		}
+
+		file_cb(&delta,0,NULL);
+	}
 
 	rc = EXIT_SUCCESS;
 out:
