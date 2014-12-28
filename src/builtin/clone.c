183b931d1b687b2ace85ec3d70b2bf0098807b06
Sebastian Bauer <mail@sebastianbauer.info>
2014-12-29 00:06:48 +0100

    Do not bypass git-aware transport when cloning.
    Otherwise, non-local URLs, e.g. beginning with https://, will be accessed
    as local files which on AmigaOS will provoke a requester.

--- src/builtin/clone.c
+++ src/builtin/clone.c
@@ -100,6 +100,11 @@ int do_clone(git_repository *repo, int argc, char **argv)
 	checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE_CREATE;
 	checkout_opts.progress_cb = checkout_progress;
 	checkout_opts.progress_payload = &pd;
+	/* Do not bypass git-aware transport as otherwise, non-local URLs, e.g.
+	 * beginning with https://, will be accessed as local files which on AmigaOS
+	 * will provoke an Requester.
+	 */
+	clone_opts.local = GIT_CLONE_NO_LOCAL;
 	clone_opts.checkout_opts = checkout_opts;
 	clone_opts.remote_callbacks.transfer_progress = &fetch_progress;
 	clone_opts.remote_callbacks.credentials = &cred_acquire;
