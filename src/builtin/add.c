1c76457f6627b95ab90aa8819c87a5f3f075f8b5
Sebastian Bauer <mail@sebastianbauer.info>
2015-01-10 13:52:05 +0100

    Use git_index_add_all() to add the files. This also works for directories.
    Partly addresses Issue #1. However, git add doesn't work yet work when
    absolute paths are used and when the root directory is used (i.e, .).

--- src/builtin/add.c
+++ src/builtin/add.c
@@ -8,15 +8,28 @@
 
 #include "strbuf.h"
 
+static int cmd_add_matched_paths_callback(const char *path, const char *matched_pathspec, void *payload)
+{
+	int *num_added = (int*)payload;
+	*num_added = *num_added + 1;
+	return 0;
+}
+
 int cmd_add(git_repository *repo, int argc, char **argv)
 {
-	int err = GIT_OK;
 	int i;
+	int err = GIT_OK;
 	int rc = EXIT_FAILURE;
-	const char *wd;
 	git_index *idx = NULL;
 	int num_added = 0;
-	int wd_len;
+	git_strarray paths;
+	const char *wd;
+
+	if (argc < 2)
+	{
+		fprintf (stderr, "USAGE: %s <paths>\n", argv[0]);
+		return -1;
+	}
 
 	if (!(wd = git_repository_workdir(repo)))
 	{
@@ -24,11 +37,13 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 		goto out;
 	}
 
-	wd_len = strlen(wd);
-
 	if ((err = git_repository_index(&idx,repo)) != GIT_OK)
 		goto out;
 
+	paths.count = argc - 1;
+	paths.strings = &argv[1];
+
+	/* Check if any path points outside the directory and bailout if so */
 	for (i=1;i<argc;i++)
 	{
 		if (argv[i][0] != '-')
@@ -36,26 +51,29 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 			/* Use a local buffer for the 2nd arg of realpath() on AmigaOS 4
 			 * doesn't accept a NULL pointer for the 2nd arg.
 			 */
-			char path_buf[PATH_MAX];
+			char path_buf[PATH_MAX+1];
 			char *path;
-
 			if ((path = realpath(argv[i],path_buf)))
 			{
-				if (!prefixcmp(path,wd))
-				{
-					int path_skip = wd_len;
-					while (path[path_skip]=='/') path_skip++;
+				int path_len;
 
-					if ((err = git_index_add_bypath(idx,&path[path_skip])) != GIT_OK)
-						goto out;
-					num_added++;
-				} else
+				/* Add directory delimiter if not already there */
+				path_len = strlen(path_buf);
+				if (path_buf[0] && path_buf[path_len-1] != '/')
 				{
-					fprintf(stderr,"%s is outside repository!\n",argv[i]);
+					path_buf[path_len] = '/';
+					path_buf[path_len+1] = 0;
+				}
+
+				if (prefixcmp(path,wd))
+				{
+					fprintf(stderr,"%s is outside the Git repository\n",argv[i]);
+					goto out;
 				}
 			} else
 			{
 				fprintf(stderr,"Couldn't determine absolute path of %s: %s!\n",argv[i], strerror(errno));
+				goto out;
 			}
 		} else
 		{
@@ -63,6 +81,9 @@ int cmd_add(git_repository *repo, int argc, char **argv)
 			goto out;
 		}
 	}
+
+	if ((err = git_index_add_all(idx, &paths, GIT_INDEX_ADD_DEFAULT, cmd_add_matched_paths_callback, &num_added)))
+		goto out;
 
 	if (num_added)
 	{
