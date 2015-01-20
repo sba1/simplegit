923ed7b2d197b10c731812a6fb11736dc7fdbd79
Sebastian Bauer <mail@sebastianbauer.info>
2015-01-20 21:00:38 +0100

    Differentiate between plumbing and porcelaine commands.

--- src/cmds.c
+++ src/cmds.c
@@ -8,39 +8,40 @@
 
 struct cmd_struct {
 	char *cmd;
+	int plumbing;
 	char *desc;
 	git_cb handler;
 };
 
 struct cmd_struct commands[] = {
-	{"add",            "Add contents of file to the index", cmd_add},
-	{"branch",         "Create, delete, or delete branches", cmd_branch},
-	{"cat-file",       "Show content of repository objects", cmd_cat_file},
-	{"checkout-index", "Copy files from the index", cmd_checkout_index},
-	{"checkout",       "Copy a branch or paths to the working directory", cmd_checkout},
-	{"clone",          "Fetch and checkout a repository into a new directory", do_clone},
-	{"commit-tree",    "Create a new commit object from a tree object", cmd_commit_tree},
-	{"commit",         "Create a new commit from the index", cmd_commit},
-	{"config",         "Get and set repository or global options", cmd_config},
-	{"diff-files",     "Show differences between files in the working directory and the index", cmd_diff_files},
-	{"diff",           "Show differences between objects", cmd_diff},
-	{"fetch",          "Fetch objects from another repository", fetch},
-	{"index-pack",     "Build the index file for an existing pack file", index_pack},
-	{"init",           "Create or reinitialize a git repository", cmd_init},
-	{"log",            "Show the commit logs", cmd_log},
-	{"ls-files",       "Show information about files in the index and the working directory", cmd_ls_files},
-	{"ls-remote",      "Show information about remote references", ls_remote},
-	{"ls-tree",        "Show information about a tree object", cmd_ls_tree},
-	{"mktag",          "Create a tag object", cmd_mktag},
-	{"merge",          "Join histories defined by commit together", cmd_merge},
-	{"push",           "Update the remote references and push associated objects", cmd_push},
-	{"read-tree",      "Read a tree object into the index", cmd_read_tree},
-	{"reset",          "Reset the HEAD or the index to a specific state", cmd_reset},
-	{"rev-list",       "Show information about commit objects", cmd_rev_list},
-	{"show",           "Show objects", cmd_show},
-	{"status",         "Summarize working directory changes", cmd_status},
-	{"update-index",   "Update the index with files from the working directory", cmd_update_index},
-	{"write-tree",     "Create a write object from the current index", cmd_write_tree},
+	{"add",            1, "Add contents of file to the index", cmd_add},
+	{"branch",         1, "Create, delete, or delete branches", cmd_branch},
+	{"cat-file",       0, "Show content of repository objects", cmd_cat_file},
+	{"checkout-index", 0, "Copy files from the index", cmd_checkout_index},
+	{"checkout",       1, "Copy a branch or paths to the working directory", cmd_checkout},
+	{"clone",          1, "Fetch and checkout a repository into a new directory", do_clone},
+	{"commit-tree",    0, "Create a new commit object from a tree object", cmd_commit_tree},
+	{"commit",         1, "Create a new commit from the index", cmd_commit},
+	{"config",         1, "Get and set repository or global options", cmd_config},
+	{"diff-files",     0, "Show differences between files in the working directory and the index", cmd_diff_files},
+	{"diff",           1, "Show differences between objects", cmd_diff},
+	{"fetch",          1, "Fetch objects from another repository", fetch},
+	{"index-pack",     0, "Build the index file for an existing pack file", index_pack},
+	{"init",           1, "Create or reinitialize a git repository", cmd_init},
+	{"log",            1, "Show the commit logs", cmd_log},
+	{"ls-files",       0, "Show information about files in the index and the working directory", cmd_ls_files},
+	{"ls-remote",      0, "Show information about remote references", ls_remote},
+	{"ls-tree",        0, "Show information about a tree object", cmd_ls_tree},
+	{"mktag",          0, "Create a tag object", cmd_mktag},
+	{"merge",          1, "Join histories defined by commit together", cmd_merge},
+	{"push",           1, "Update the remote references and push associated objects", cmd_push},
+	{"read-tree",      0, "Read a tree object into the index", cmd_read_tree},
+	{"reset",          1, "Reset the HEAD or the index to a specific state", cmd_reset},
+	{"rev-list",       0, "Show information about commit objects", cmd_rev_list},
+	{"show",           1, "Show objects", cmd_show},
+	{"status",         1, "Summarize working directory changes", cmd_status},
+	{"update-index",   0, "Update the index with files from the working directory", cmd_update_index},
+	{"write-tree",     0, "Create a write object from the current index", cmd_write_tree},
 };
 
 void print_commands_overview()
@@ -48,10 +49,17 @@ void print_commands_overview()
 	size_t i;
 	size_t max = sizeof(commands) / sizeof(commands[0]);
 
-	fprintf(stderr, "Following commands are supported:\n");
+	fprintf(stderr, "Following plumbing commands are supported:\n");
 	for (i = 0; i < max; i++)
 	{
-		fprintf(stderr, "  %-15s %s\n", commands[i].cmd, commands[i].desc);
+		if (commands[i].plumbing)
+			fprintf(stderr, "  %-15s %s\n", commands[i].cmd, commands[i].desc);
+	}
+	fprintf(stderr, "Following porcelaine commands are supported:\n");
+	for (i = 0; i < max; i++)
+	{
+		if (!commands[i].plumbing)
+			fprintf(stderr, "  %-15s %s\n", commands[i].cmd, commands[i].desc);
 	}
 }
 
