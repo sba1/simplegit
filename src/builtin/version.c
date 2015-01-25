c3c7890afb959bf7474df3d118826dc9b9b376af
Sebastian Bauer <mail@sebastianbauer.info>
2015-01-25 14:01:46 +0100

    Display libgit2 and openssl version when invoking the version command.

--- src/builtin/version.c
+++ src/builtin/version.c
@@ -2,9 +2,16 @@
 
 #include <stdio.h>
 
+#include <git2.h>
+#include <openssl/crypto.h>
+
 int cmd_version(git_repository *repo, int argc, char **argv)
 {
+	int major, minor, rev;
+
+	git_libgit2_version(&major, &minor, &rev);
 	printf("sgit version 0.%s\n", SIMPLEGIT_REVISION_STRING);
+	printf("using libgit2 %d.%d.%d and %s\n", major, minor, rev, SSLeay_version(SSLEAY_VERSION));
 
 	return EXIT_SUCCESS;
 }
