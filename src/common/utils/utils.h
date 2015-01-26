fe8241f54b1aec0e926c3cf0b515c41f0eab5c90
Sebastian Bauer <mail@sebastianbauer.info>
2015-01-26 20:34:56 +0100

    Removed some unused code.

--- src/common/utils/utils.h
+++ src/common/utils/utils.h
@@ -15,18 +15,7 @@ void *xmemdupz(const void *data, size_t len);
 char *xstrndup(const char *str, size_t len);
 void *xrealloc(void *ptr, size_t size);
 void *xcalloc(size_t nmemb, size_t size);
-//void *xmmap(void *start, size_t length, int prot, int flags, int fd, off_t offset);
 ssize_t xread(int fd, void *buf, size_t len);
-ssize_t xwrite(int fd, const void *buf, size_t len);
-ssize_t read_in_full(int fd, void *buf, size_t count);
-ssize_t write_in_full(int fd, const void *buf, size_t count);
-/*int xdup(int fd);
-FILE *xfdopen(int fd, const char *mode);
-int xmkstemp(char *template);
-int xmkstemp_mode(char *template, int mode);
-*/
-
-const char *prefix_filename(const char *pfx, int pfx_len, const char *arg);
 
 /**
  * Join two paths together. Takes care of properly fixing the
