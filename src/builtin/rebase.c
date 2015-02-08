c29ce70ec28434dfd752625bd952473f874bd497
Sebastian Bauer <mail@sebastianbauer.info>
2015-02-08 22:48:27 +0100

    Perform the rebase options.

--- src/builtin/rebase.c
+++ src/builtin/rebase.c
@@ -50,6 +50,12 @@ int cmd_rebase(git_repository *repo, int argc, char **argv)
 			goto out;
 	} else
 	{
+		git_rebase_operation *oper;
+		git_checkout_options checkout_opts = GIT_CHECKOUT_OPTIONS_INIT;
+		git_rebase_options rebase_opts = GIT_REBASE_OPTIONS_INIT;
+
+		checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
+
 		if ((err = git_reference_dwim(&upstream_ref, repo, upstream_str)))
 			goto out;
 		if ((err = git_annotated_commit_from_ref(&upstream, repo, upstream_ref)))
@@ -61,6 +67,19 @@ int cmd_rebase(git_repository *repo, int argc, char **argv)
 			goto out;
 
 		if ((err = git_rebase_init(&rebase, repo, branch, upstream, NULL, NULL, NULL)))
+			goto out;
+
+		while (!(err = git_rebase_next(&oper, rebase, &checkout_opts)))
+		{
+			git_oid oid;
+			if ((err = git_rebase_commit(&oid, rebase, NULL, sig, NULL, NULL)))
+				goto out;
+		}
+
+		if (err != GIT_ITEROVER && err != GIT_OK)
+			goto out;
+
+		if ((err = git_rebase_finish(rebase, sig, &rebase_opts)))
 			goto out;
 	}
 out:
