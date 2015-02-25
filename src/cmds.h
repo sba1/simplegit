2b6d46c43fadc735d88af5f9222359bd0339185e
Sebastian Bauer <mail@sebastianbauer.info>
2015-02-25 21:27:30 +0100

    Moved all the builtin include refs to the source file.
    The functions that are defined therin, are not needed anywhere else.

--- src/cmds.h
+++ src/cmds.h
@@ -1,34 +1,6 @@
 #ifndef CMDS_H
 #define CMDS_H
 
-// original source : https://github.com/vfr-nl/git2
-
-#include <git2.h>
-#include "builtin/add.h"
-#include "builtin/branch.h"
-#include "builtin/cat-file.h"
-#include "builtin/checkout.h"
-#include "builtin/checkout-index.h"
-#include "builtin/commit.h"
-#include "builtin/commit-tree.h"
-#include "builtin/config.h"
-#include "builtin/diff.h"
-#include "builtin/diff-files.h"
-#include "builtin/init.h"
-#include "builtin/log.h"
-#include "builtin/ls-files.h"
-#include "builtin/ls-tree.h"
-#include "builtin/merge.h"
-#include "builtin/mktag.h"
-#include "builtin/push.h"
-#include "builtin/read-tree.h"
-#include "builtin/rev-list.h"
-#include "builtin/show.h"
-#include "builtin/status.h"
-#include "builtin/update-index.h"
-#include "builtin/write-tree.h"
-#include "builtin/version.h"
-
 #include "common.h"
 
 /**
