186a8624409b0f9410d56672a716bfb6998191d4
Sebastian Bauer <mail@sebastianbauer.info>
2015-02-28 21:09:54 +0100

    Removed some obsolete stuff.

--- src/common/git-compat-util.h
+++ src/common/git-compat-util.h
@@ -4,8 +4,6 @@
 #include <limits.h>
 #include <string.h>
 
-#define _FILE_OFFSET_BITS 64
-
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 #define bitsizeof(x)  (CHAR_BIT * sizeof(x))
 
@@ -27,56 +25,19 @@
 #define unsigned_add_overflows(a, b) \
     ((b) > maximum_unsigned_value_of_type(a) - (a))
 
-#define MSB(x, bits) ((x) & TYPEOF(x)(~0ULL << (bitsizeof(x) - (bits))))
-#define HAS_MULTI_BITS(i)  ((i) & ((i) - 1))  /* checks if an integer has more than 1 bit set */
-
-#define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
-
 /* Approximation of the length of the decimal representation of this type. */
 #define decimal_length(x)	((int)(sizeof(x) * 2.56 + 0.5) + 1)
-
-/* On most systems <limits.h> would have given us this, but
- * not on some systems (e.g. GNU/Hurd).
- */
-#ifndef PATH_MAX
-#define PATH_MAX 4096
-#endif
-
-#ifndef PRIuMAX
-#define PRIuMAX "llu"
-#endif
-
-#ifndef PRIu32
-#define PRIu32 "u"
-#endif
-
-#ifndef PRIx32
-#define PRIx32 "x"
-#endif
-
-#ifndef PRIo32
-#define PRIo32 "o"
-#endif
 
 #ifndef is_dir_sep
 #define is_dir_sep(c) ((c) == '/')
 #endif
 
-#if __HP_cc >= 61000
-#define NORETURN __attribute__((noreturn))
-#define NORETURN_PTR
-#elif defined(__GNUC__)
+#if defined(__GNUC__)
 #define NORETURN __attribute__((__noreturn__))
 #define NORETURN_PTR __attribute__((__noreturn__))
-#elif defined(_MSC_VER)
-#define NORETURN __declspec(noreturn)
-#define NORETURN_PTR
 #else
 #define NORETURN
 #define NORETURN_PTR
-#ifndef __attribute__
-#define __attribute__(x)
-#endif
 #endif
 
 static inline const char *skip_prefix(const char *str, const char *prefix)
