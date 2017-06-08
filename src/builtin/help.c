5c0e7a9366d22627c91f4b7d97ef10201ac0fd0e
Sebastian Bauer <mail@sebastianbauer.info>
2017-06-08 23:17:25 +0200

    Fixed few warnings about unused parameters.

--- src/builtin/help.c
+++ src/builtin/help.c
@@ -9,6 +9,9 @@
 
 int cmd_help(git_repository *repo, int argc, char **argv)
 {
+	(void)repo;
+	(void)argc;
+
 	fprintf(stdout,"Usage: %s <command>\n", argv[0]);
 	print_commands_overview();
 	return EXIT_SUCCESS;
