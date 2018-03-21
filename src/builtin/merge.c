6a67b0d4e5de6a4ed9a6ae767714bd16b283cd79
Sebastian Bauer <mail@sebastianbauer.info>
2018-03-21 22:04:26 +0100

    Add --ff-only support to merge.

--- src/builtin/merge.c
+++ src/builtin/merge.c
@@ -17,6 +17,7 @@ int cmd_merge(git_repository *repo, int argc, char **argv)
 	int err = GIT_OK;
 	int rc = EXIT_FAILURE;
 	int autocommit = 1;
+	int ff_only = 0;
 
 	git_merge_analysis_t analysis;
 	git_merge_preference_t preference;
@@ -38,13 +39,14 @@ int cmd_merge(git_repository *repo, int argc, char **argv)
 
 	if (argc < 2)
 	{
-		fprintf (stderr, "USAGE: %s <commit> [--no-commit]\n", argv[0]);
+		fprintf (stderr, "USAGE: %s <commit> [--no-commit] [--ff-only]\n", argv[0]);
 		return -1;
 	}
 
 	for (i=1; i < argc; i++)
 	{
 		if (!strcmp(argv[i], "--no-commit")) autocommit = 0;
+		else if (!strcmp(argv[i], "--ff-only")) ff_only = 1;
 		else if (!commit_str) commit_str = argv[i];
 		else
 		{
@@ -72,6 +74,12 @@ int cmd_merge(git_repository *repo, int argc, char **argv)
 		goto out;
 
 	checkout_options.checkout_strategy = GIT_CHECKOUT_SAFE;
+
+	if (ff_only && !(analysis & GIT_MERGE_ANALYSIS_FASTFORWARD))
+	{
+		fprintf(stderr, "No fast forward merge possible\n");
+		goto out;
+	}
 
 	if (analysis & GIT_MERGE_ANALYSIS_FASTFORWARD && preference != GIT_MERGE_PREFERENCE_NO_FASTFORWARD)
 	{
